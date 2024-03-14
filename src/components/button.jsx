7b78ba3203435de614c466d891a1cd94f7e676e4
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-14 16:45:17 +0200

    button updated

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 
 export class Counter extends Component {
     static defaultProps = {
         step: 1,
     };
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    //     this.handleDecrement = this.handleDecrement.bind(this);
+    // }
+
+    handleIncrement = evt => {
+        console.log("Клікнуто на кнопці Increment button!", evt);
+        console.log("this.props: ", this.props);
+    }
+
+    handleDecrement = evt => {
+        console.log("Клікнуто на кнопці Decrement button!", evt);
+        console.log("this.props: ", this.props);
+    }
 
     render() {
         const { step } = this.props;
@@ -12,11 +27,24 @@ export class Counter extends Component {
         return (
             <div>
                 <span>0</span>
-                <button type="button">Increment by {step}</button>
-                <button type="button">Increment by {step}</button>
+                <button
+                    type="button"
+                    onClick={this.handleIncrement}
+                    // onClick={evt => {
+                    //     console.log("Клікнуто на кнопці Increment button!", evt);
+                    //     console.log("this.props: ", this.props);
+                    // }}
+                        >Increment by {step}</button>
+                <button
+                    type="button"
+                    onClick={this.handleDecrement}
+                    // onClick={evt => {
+                    //     console.log("Клікнуто на кнопці Decrement button!", evt);
+                    //     console.log("this.props: ", this.props);                        
+                    // }}
+                        >Increment by {step}</button>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Counter step={5} />, document.getElementById('root'));
