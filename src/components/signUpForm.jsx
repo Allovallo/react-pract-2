38f1ca4e0a7513f72a3a7fb96d1fb271c23a89e1
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-15 18:27:32 +0200

    form updated

--- src/components/signUpForm.jsx
+++ src/components/signUpForm.jsx
@@ -1,23 +1,36 @@
 import { React, Component } from "react";
 
-export class SignUpForm extends Component {
-    state = {
-        login: '',
-    };
+const INITIAL_STATE = {
+    login: '',
+    email: '',
+    password: '',
+}
 
-    handleChange = e => {
-        this.setState({login: e.target.value})
+export class SignUpForm extends Component {
+    state = {...INITIAL_STATE };
+
+    handleChange = evt => {
+        // this.setState({login: e.target.value})
+        const { name, value } = evt.target;
+        this.setState({[name]: value})
     }
 
     handleSubmit = evt => {
         evt.preventDefault();
-        console.log(`Signed up as: ${this.state.login}`);
+        // console.log(`Signed up as: ${this.state.login}`);
+        const { login, email, password } = this.state;
+        console.log(`Login: ${login}, Email: ${email}, Password: ${password}`)
 
-        this.props.onSubmit({...this.state})
+        this.props.onSubmit({ ...this.state })
+        this.reset();
+    }
+
+    reset = () => {
+        this.setState({...INITIAL_STATE })
     }
 
     render() {
-        const { login } = this.state;
+        const { login, email, password } = this.state;
 
         return (
             <form onSubmit={this.handleSubmit}>
@@ -26,7 +39,30 @@ export class SignUpForm extends Component {
                     <input
                         type="text"
                         placeholder="Enter login"
+                        name="login"
                         value={login}
+                        onChange={this.handleChange}
+                    />
+                </label>
+
+                <label>
+                    Email
+                    <input
+                        type="email"
+                        placeholder="Enter email"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                </label>
+
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        placeholder="Enter password"
+                        name="password"
+                        value={password}
                         onChange={this.handleChange}
                     />
                 </label>
